a637a500df10efbd269aaa90c723003255c1a11c
Kauê Corrêa <kauehenrique08@gmail.com>
2023-09-27 12:32:01 -0300

    Add files via upload

--- if_else3/src/if_else3/Main.java
+++ if_else3/src/if_else3/Main.java
@@ -10,6 +10,17 @@ public class Main {
 		int numero;
 		int numero2;
 		
+		System.out.println("Insira 2 valores");
+		numero = sc.nextInt();
+		numero2 = sc.nextInt();
+		
+		if (numero % numero2 == 0 || numero2 % numero == 0) {
+			System.out.println("Sao multiplos");	
+		}
+		else {
+			System.out.println("nao sao multiplos");
+		}
+		
 		sc.close();
 	}
 
