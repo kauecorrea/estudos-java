34ef615f75c7f9f236f09d2e26911f87961bff8b
Kauê Corrêa <kauehenrique08@gmail.com>
2023-10-19 13:59:07 -0300

    Add files via upload

---
+++ while_exec01/src/while_exec01/Main.java
@@ -0,0 +1,23 @@
+package while_exec01;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int senha = sc.nextInt();
+		
+		while (senha != 2002) {
+			System.out.println("senha incorreta");
+			senha = sc.nextInt();
+		}
+		
+		System.out.println("Acesso permitido");
+		
+		
+		sc.close();
+
+	}
+
+}
