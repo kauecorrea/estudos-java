0486989896575f730d9e110db68b5309ec72fc1f
Kauê Corrêa <kauehenrique08@gmail.com>
2024-04-05 11:16:42 -0300

    Add files via upload
    Aprendendo como organizar um projeto com pacotes e classes!

---
+++ clinica/src/adm/Agenda.java
@@ -0,0 +1,10 @@
+package adm;
+
+public class Agenda {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ clinica/src/tela/TelaAgendamento.java
@@ -0,0 +1,10 @@
+package tela;
+
+public class TelaAgendamento {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
