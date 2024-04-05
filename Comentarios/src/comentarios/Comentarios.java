f2038dec4a398c57f6048e98d0bbba917897be1c
Kauê Corrêa <kauehenrique08@gmail.com>
2024-04-05 12:39:22 -0300

    Add files via upload
    Formas eficientes de comentar!

---
+++ Comentarios/src/comentarios/Comentarios.java
@@ -0,0 +1,36 @@
+package comentarios;
+
+/**
+ * Essa classe representa... (podemos explicar sobre esta classe) 
+ * JAVADOC
+ * 
+ * 
+ * @author Kauê Corrêa <kauehenrique08@gmail.com>
+ * @since JDK 17.0
+ * @see veja mais sobre...
+ */
+
+public class Comentarios {
+
+	/** COMENTARIOS JAVADOC (serve para dar informações adicionais 
+	 * para outra pessoa que for ler o codigo)
+	 * 
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// o "barra barra" serve para comentar uma linha apenas
+		System.out.println("primeiro programa parte #01");
+		
+		/*
+		 desse jeito podemos comentar tudo que estiver dentro de "/*" 
+		 
+		 */
+		
+		System.out.println("primeiro programa parte #02");
+		
+//		System.out.println("voce pode comentar uma linha usando o ctrl + /");
+	}
+
+}
