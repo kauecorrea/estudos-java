01851723c99bfe70df8ee8db48abca2f867e06e6
Kauê Corrêa <kauehenrique08@gmail.com>
2024-04-16 13:44:57 -0300

    Add files via upload

---
+++ exercicios_fundamentos/src/Fundamentos/Wrapper.java
@@ -0,0 +1,16 @@
+package Fundamentos;
+
+public class Wrapper {
+
+	public static void main(String[] args) {
+		
+		//byte
+		Byte b = 100;
+		Short s = 1000;
+		Integer i = 10000;
+		Long l = 100000;
+		
+		
+	}
+
+}
