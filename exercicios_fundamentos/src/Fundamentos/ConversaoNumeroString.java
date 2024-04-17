f34bcf9c1d49f797fd526003eb4b74484552156a
Kauê Corrêa <kauehenrique08@gmail.com>
2024-04-17 13:48:58 -0300

    Add files via upload

--- exercicios_fundamentos/src/Fundamentos/ConversaoNumeroString.java
+++ exercicios_fundamentos/src/Fundamentos/ConversaoNumeroString.java
@@ -3,8 +3,15 @@ package Fundamentos;
 public class ConversaoNumeroString {
 
 	public static void main(String[] args) {
+		//uma forma de converter integer para string
+		Integer num1 = 10000;
+		System.out.println(num1.toString().length()); // .length serve para saber quantos caracteres tem
 		
-
+		//outra forma de converter
+		int num2 = 100000;
+		System.out.println(Integer.toString(num2).length());
+		
+		
 	}
 
 }
