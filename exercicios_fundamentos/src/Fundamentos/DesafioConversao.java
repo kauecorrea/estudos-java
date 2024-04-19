7d7174910da6e8ec156e530de8e86be41cf5ea9d
Kauê Corrêa <kauehenrique08@gmail.com>
2024-04-19 10:28:09 -0300

    Add files via upload

--- exercicios_fundamentos/src/Fundamentos/DesafioConversao.java
+++ exercicios_fundamentos/src/Fundamentos/DesafioConversao.java
@@ -1,31 +1,33 @@
 package Fundamentos;
 
+import java.util.Locale;
 import java.util.Scanner;
 
-
-
 public class DesafioConversao {
+	
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner (System.in);
 		
-		System.out.println("insira seu salario de janeiro: ");
+		System.out.println(Locale.getDefault());
 		
-		String janeiro = sc.nextLine();
+		System.out.print("insira seu salario de janeiro: ");
 		
-		System.out.println("Insira seu salario de fevereiro: ");
-		String fevereiro = sc.nextLine();
+		String janeiro =  sc.next().replace(",", ".");
 		
-		System.out.println("Insira seu salario de março: ");
-		String março = sc.nextLine();
+		System.out.print("\nInsira seu salario de fevereiro: ");
+		String fevereiro = sc.next().replace(",", ".");
+		
+		System.out.print("\nInsira seu salario de março: ");
+		String março = sc.next().replace(",", ".");
 		
 		double salario1 = Double.parseDouble(janeiro);
 		double salario2 = Double.parseDouble(fevereiro);
 		double salario3 = Double.parseDouble(março);
 		
-		double soma = (janeiro + fevereiro) + março;
+		double soma = salario1 + salario2 + salario3;
 		System.out.println("a soma é: " + soma);
-		System.out.println("a média é: " + soma / 2);
+		System.out.println("a média é: " + soma / 3);
 		sc.close();
 	}
 
