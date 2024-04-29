50bd7cf0bfc017e235509fa0b03bd27b261ad480
Kauê Corrêa <kauehenrique08@gmail.com>
2024-04-29 12:32:14 -0300

    Add files via upload

---
+++ exercicios_fundamentos/src/Fundamentos/operadores/DesafioLogicos.java
@@ -0,0 +1,13 @@
+package Fundamentos.operadores;
+
+public class DesafioLogicos {
+
+	public static void main(String[] args) {
+		
+		// Trabalho na terça (V ou F)
+		// Trabalho na quinta (V ou F)
+		
+		
+	}
+
+}
--- exercicios_fundamentos/src/Fundamentos/operadores/Logicos.java
+++ exercicios_fundamentos/src/Fundamentos/operadores/Logicos.java
@@ -12,25 +12,29 @@ public class Logicos {
 		System.out.println(!!condicao1);
 		System.out.println(!condicao2);
 		
+		//todos tem que ser verdadeiro (AND , &&)
 		System.out.println("\nTabela verdade E");
 		System.out.println(true && true);
 		System.out.println(true && false);
 		//System.out.println(false && true);
 		//System.out.println(false && false);
 		
-		
+		// um sendo verdadeiro ja dá true (OR, ||)
 		System.out.println("\nTabela Verdade OU");
 		//System.out.println(true || true);
 		//System.out.println(true || false);
 		System.out.println(false || true);
 		System.out.println(false || false);
 		
+		//só pode ter um verdadeiro, ou é um ou é outro. pegou a parada? (XOR, ^)
 		System.out.println("\nTabela Verdade OU Exclusivo (XOR)");
 		System.out.println(true ^ true);
 		System.out.println(true ^  false);
 		System.out.println(false ^ true);
 		System.out.println(false ^ false);
 		
+		// ele nega. se for verdadeiro e colocar ele, ele vira falso
+		// se for falso e colocar ele, ele vira verdadeiro (NOT, !)
 		System.out.println("\nTabela Verdade NOT");
 		System.out.println(!true);
 		System.out.println(!false);
